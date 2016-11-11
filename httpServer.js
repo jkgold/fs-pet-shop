c0915faf4b1e8723ef082b861f3448e44edda7fd
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-11-11 14:11:14 -0700

    express server working

--- httpServer.js
+++ httpServer.js
@@ -1,14 +1,19 @@
 'use strict';
-
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(res, req){
-  res.send("this is a web page");
+app.get('/pets', function(req, res){
+  fs.readFile("./pets.json", "utf8", function(err, data) {
+    if (err){
+      throw err
+    }
+
+  });
 });
 
-app.listen('3000', function(){
+app.listen(3000, function(){
   console.log("listening for port 3000");
 })
