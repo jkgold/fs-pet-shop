996bd5776ea067046561c3e787844a305fa3c883
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2016-11-14 16:24:40 -0700

    working with 3000

--- httpServer.js
+++ httpServer.js
@@ -2,10 +2,20 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
 app.get('/pets', function(req, res){
+  fs.readFile("./pets.json", "utf8", function(err, data) {
+    if (err){
+      throw err
+    }
+      res.send(JSON.parse(data));
+
+  });
+});
+app.get('/pets/0', function(req, res){
   fs.readFile("./pets.json", "utf8", function(err, data) {
     if (err){
       throw err
